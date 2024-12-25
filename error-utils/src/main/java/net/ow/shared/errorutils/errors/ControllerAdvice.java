6978c97f2a580a446fe8e452db843edd5770aa85
Oliver <hohang210@gmail.com>
2024-12-26 00:56:12 +0800

    [FIX] Fix controller advice constructor injection issue

--- error-utils/src/main/java/net/ow/shared/errorutils/errors/ControllerAdvice.java
+++ error-utils/src/main/java/net/ow/shared/errorutils/errors/ControllerAdvice.java
@@ -4,12 +4,12 @@ import java.nio.file.AccessDeniedException;
 import javax.naming.AuthenticationException;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintViolationException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import net.ow.shared.errorutils.dto.APIResponse;
 import net.ow.shared.errorutils.dto.Error;
 import net.ow.shared.errorutils.mapper.ErrorMapper;
 import net.ow.shared.errorutils.util.LocaleMessageSource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -20,19 +20,14 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @Component
 @RestControllerAdvice
+@RequiredArgsConstructor
 public class ControllerAdvice {
-    private static final Logger log = LoggerFactory.getLogger(ControllerAdvice.class);
-
     private final LocaleMessageSource messages;
 
     private final ErrorMapper errorMapper;
-
-    public ControllerAdvice(final LocaleMessageSource messages, final ErrorMapper errorMapper) {
-        this.messages = messages;
-        this.errorMapper = errorMapper;
-    }
 
     /**
      * Handles application-specific exceptions

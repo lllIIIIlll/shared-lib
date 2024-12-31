04c129f312f59912e8bdee194049e6cc6009b39f
hohang210 <35669859+hohang210@users.noreply.github.com>
2024-12-31 19:20:09 +0800

    [FEAT] Add YAML property source factory (#14)
    * [FEAT] Add YAML property source factory
    
    * [TEST] Update configuration to exclude unit test for YAMLPropertySourceFactory

---
+++ property-utils/src/main/java/net/ow/shared/propertyutils/factory/YAMLPropertySourceFactory.java
@@ -0,0 +1,30 @@
+package net.ow.shared.propertyutils.factory;
+
+import io.micrometer.common.lang.NonNullApi;
+import io.micrometer.common.lang.Nullable;
+import java.util.Objects;
+import java.util.Properties;
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
+import org.springframework.core.env.PropertiesPropertySource;
+import org.springframework.core.env.PropertySource;
+import org.springframework.core.io.support.EncodedResource;
+import org.springframework.core.io.support.PropertySourceFactory;
+
+@NonNullApi
+public class YAMLPropertySourceFactory implements PropertySourceFactory {
+    private static final String ERROR_MESSAGE = "Failed to setup yaml property source factory";
+
+    @Override
+    public PropertySource<?> createPropertySource(@Nullable String name, EncodedResource resource) {
+        YamlPropertiesFactoryBean factory = new YamlPropertiesFactoryBean();
+        factory.setResources(resource.getResource());
+
+        Properties properties = factory.getObject();
+        assert properties != null : ERROR_MESSAGE;
+
+        return new PropertiesPropertySource(
+                Objects.requireNonNullElseGet(
+                        name, () -> Objects.requireNonNull(resource.getResource().getFilename())),
+                properties);
+    }
+}

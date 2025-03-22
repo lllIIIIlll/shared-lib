cadccdb0ada04970111814f4ba34678c7d73911d
Oliver <hohang210@gmail.com>
2025-03-22 18:23:57 +0800

    [FEAT] Implement a QueryMapEncoder to convert field name to expected parameter name

---
+++ server-common/src/test/java/net/ow/shared/common/server/encoder/CommonQueryMapEncoderTest.java
@@ -0,0 +1,67 @@
+package net.ow.shared.common.server.encoder;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Map;
+import lombok.Data;
+import net.ow.shared.common.server.annotation.QueryParameter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CommonQueryMapEncoderTest {
+    private CommonQueryMapEncoder encoder;
+
+    @BeforeEach
+    public void setUp() {
+        encoder = new CommonQueryMapEncoder();
+    }
+
+    @Test
+    void encodeTest_whenFieldWithQueryNameAnnotation_thenUsesAnnotationValueAsParameterName() {
+        String value = "value";
+
+        AnnotatedQueryParameter parameter = new AnnotatedQueryParameter();
+        parameter.setParameter(value);
+
+        Map<String, Object> result = encoder.encode(parameter);
+
+        assertEquals(1, result.size());
+        assertEquals(value, result.get("customName"));
+    }
+
+    @Test
+    void encodeTest_whenFieldWithoutQueryNameAnnotation_thenUsesFieldNameAsParameterName() {
+        String value = "value";
+
+        PlainQueryParameter parameter = new PlainQueryParameter();
+        parameter.setParameter(value);
+
+        Map<String, Object> result = encoder.encode(parameter);
+
+        assertEquals(1, result.size());
+        assertEquals(value, result.get("parameter"));
+    }
+
+    @Test
+    void encodeTest_whenFieldWithNullValue_thenStoresNullInMap() {
+        AnnotatedQueryParameter parameter = new AnnotatedQueryParameter();
+        parameter.setParameter(null);
+
+        Map<String, Object> result = encoder.encode(parameter);
+
+        assertEquals(1, result.size());
+        assertTrue(result.containsKey("customName"));
+        assertNull(result.get("customName"));
+    }
+
+    @Data
+    private static class AnnotatedQueryParameter {
+        @QueryParameter(name = "customName")
+        private String parameter;
+    }
+
+    @Data
+    private static class PlainQueryParameter {
+        private String parameter;
+    }
+}
